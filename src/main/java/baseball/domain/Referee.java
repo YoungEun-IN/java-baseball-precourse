72d2798190db2c6fd97416b83bc942d1be140540
Youngeun IN <yyoungeunin@gmail.com>
2022-04-16 20:40:04 +0900

    fix: 텍스트 출력 포맷 수정

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -4,7 +4,7 @@ public class Referee {
 
     public static String sign(BaseBall baseBall) {
         if (baseBall.ball() > 0 && baseBall.strike() > 0) {
-            return baseBall.ball() + "볼 " + baseBall.strike() + "스트라이크 ";
+            return baseBall.ball() + "볼 " + baseBall.strike() + "스트라이크";
         }
         if (baseBall.strike() > 0) {
             return baseBall.strike() + "스트라이크";
