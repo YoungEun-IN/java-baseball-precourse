a7e8abfb32f960e9ca7d1f5d0b09e3068cbd5e47
Youngeun IN <yyoungeunin@gmail.com>
2022-04-16 20:25:28 +0900

    feat: 판단 결과를 외쳐주는 Referee 클래스 추가

---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,17 @@
+package baseball.domain;
+
+public class Referee {
+
+    public static String sign(BaseBall baseBall) {
+        if (baseBall.ball() > 0 && baseBall.strike() > 0) {
+            return baseBall.ball() + "볼 " + baseBall.strike() + "스트라이크 ";
+        }
+        if (baseBall.strike() > 0) {
+            return baseBall.strike() + "스트라이크";
+        }
+        if (baseBall.ball() > 0) {
+            return baseBall.ball() + "볼";
+        }
+        return "낫싱";
+    }
+}
