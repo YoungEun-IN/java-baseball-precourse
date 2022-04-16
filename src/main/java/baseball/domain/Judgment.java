d13d618c4d5b4ee3dca81cec245e2d96e84036af
Youngeun IN <yyoungeunin@gmail.com>
2022-04-16 20:25:05 +0900

    feat: 입력한 값을 판단해주는 Judgment 추가

---
+++ src/main/java/baseball/domain/Judgment.java
@@ -0,0 +1,37 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judgment {
+
+    public BaseBall compareNumber(List<Integer> computer, List<Integer> player) {
+        int ball = this.containNumberCount(computer, player);
+        int strike = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            strike += this.samePlace(computer, i, player.get(i));
+        }
+        return new BaseBall(strike, ball - strike);
+    }
+
+    private int containNumberCount(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+        for (Integer integer : player) {
+            result += containNumber(computer, integer);
+        }
+        return result;
+    }
+
+    private int samePlace(List<Integer> computer, int location, int number) {
+        if (computer.get(location) == number) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private int containNumber(List<Integer> computer, int playerNumber) {
+        if (computer.contains(playerNumber)) {
+            return 1;
+        }
+        return 0;
+    }
+}
