e69dbfef74eb4e25d1c713cd6d5e295639380c5c
Youngeun IN <yyoungeunin@gmail.com>
2022-04-16 20:26:44 +0900

    feat: 야구게임 기능을 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.domain.BaseBallGame;
+import baseball.domain.BassBallNumberGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> answer;
+        do {
+            answer = BassBallNumberGenerator.createRandomNumbers();
+        } while(BaseBallGame.playBall(answer));
     }
 }
